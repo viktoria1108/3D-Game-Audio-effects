b6427958da8517bc0a1c301ce45f19781f6c8485
Vik_toria <72942985+viktoria1108@users.noreply.github.com>
2022-11-11 18:30:41 +0200

    FullGame

---
+++ 3D-Game-Audio/Assets/Scripts/Steps.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Steps : MonoBehaviour
+{
+    FirstPersonController myController;
+    public AudioClip[] stepsWater;
+    public AudioClip[] stepsTerrain;
+
+    void Start()
+    {
+        myController = GetComponent<FirstPersonController>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,-transform.up,out hit, 2))
+        {
+            if (hit.collider.tag == ("SurfaceWater"))
+            {
+                myController.m_FootstepSounds = stepsWater;
+            }
+            else
+            {
+                myController.m_FootstepSounds = stepsTerrain;
+            }
+        }
+    }
+}
